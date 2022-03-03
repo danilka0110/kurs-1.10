46468e7043bdc8b164516ba5f5d49fafcba04d51
Daniil <danil-ryumin888@yandex.ru>
2022-03-03 14:45:29 +0300

    lesson 1.12

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 "use strict";   
 
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ''),
+      b = prompt("Один из последних просмотренных фильмов?", ''),
+      c = prompt("Один из последних просмотренных фильмов?", ''),
+      d = prompt("Один из последних просмотренных фильмов?", '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
